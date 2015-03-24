af4b006537877711fe4aaae322b8cc4c5b32ce8e
xterminal86 <xterminal86@gmail.com>
2015-03-24 20:11:45 +0300

    blank for ship fragmentation explosion

--- include/explosion.h
+++ include/explosion.h
@@ -2,15 +2,32 @@
 #define EXPLOSION_H
 
 #include "png_loader.h"
+#include "vector2.h"
+#include "util.h"
+
+struct Fragment
+{
+  double Angle;
+  double RandomAngleIncrement;
+
+  SDL_Rect SrcRect;
+  SDL_Rect DstRect;
+  Vector2 Position;
+  Vector2 Direction;
+};
 
 class Explosion
 {
   public:
     Explosion();
     virtual ~Explosion();
+    void Init(PNGLoader* imageRef, int fragmentsNum);
   protected:
   private:
     PNGLoader* _imageRef;
+    int _fragmentsNum;
+
+    std::vector<Fragment> _fragments;
 };
 
 #endif // EXPLOSION_H
--- src/explosion.cpp
+++ src/explosion.cpp
@@ -9,3 +9,25 @@ Explosion::~Explosion()
 {
   //dtor
 }
+
+void Explosion::Init(PNGLoader* imageRef, int fragmentsNum)
+{
+  _imageRef = imageRef;
+  _fragmentsNum = fragmentsNum;
+
+  for (int i = 0; i < _fragmentsNum; i++)
+  {
+    Fragment f;
+
+    f.Angle = 0.0;
+    f.RandomAngleIncrement = Util::CreateRandomRotation();
+    Util::CreateRandomDirection(f.Direction);
+
+    /*
+    f.SrcRect.x = 0;
+    f.SrcRect.y = 0;
+    f.SrcRect.w = imageRef->Width();
+    f.SrcRect.h = imageRef->Height();
+    */
+  }
+}
